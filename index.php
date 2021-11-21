65f4ac0fa2f79b5330acc558e07c82f0aff1f408
AishwaryaKyasari <92700697+AishwaryaKyasari@users.noreply.github.com>
2021-11-21 07:56:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sparks Foundation Bank</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1 style="color:whitesmoke">
+            <a href="index.php"><img src="the-sparks-fondation logo.png">Sparks Foundation Bank</a>
+        </h1>
+        <nav>
+            <a class="link" href="index.php">Home</a>
+            <a class="link" href="view_customers.php">View Customers</a>
+            <a class="link" href="transaction_history.php">Transaction History</a>
+        </nav>
+    </header>
+</body>
+</html>
